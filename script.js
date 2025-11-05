4d8038f11dc8166179159a2484c17041f628e23b
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2025-11-05 20:09:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function getFormvalue() {
     //Write your code here
 
+	event.preventDefault();
+
+	let fname = document.querySelector("input[name='fname']").value.trim();
+	let lname = document.querySelector("input[name = 'lname']").value.trim();
+
+	if( !fname || !lname){
+		alert("Please enter both first and last name");
+		return;
+	}
+
+	let fullname = `${fname} ${lname}`;
+
+	alert(`${fullname}`);
 }
+
