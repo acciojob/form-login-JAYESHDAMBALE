9d0ce04f960c7108ea48cec005b0ed9b8ef88ce3
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2025-11-05 20:27:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
 
-	event.preventDefault();
+	//event.preventDefault();
 
 	let firstName = document.querySelector("input[name='fname']").value.trim();
 	let lastname = document.querySelector("input[name='lname']").value.trim();
